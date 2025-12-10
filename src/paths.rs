e5fa9d25fd665703ce497c00361884dd5ba577fa
Ray <raymond.gabriel@gmail.com>
2025-12-10 15:52:11 +1300

    add format_bytes

--- src/paths.rs
+++ src/paths.rs
@@ -1,5 +1,23 @@
 ﻿use std::path::{Component, Path, PathBuf};
 
+pub fn format_bytes(bytes:u64) -> String {
+	if bytes < 1_024 {
+		return format!("{}B", bytes)
+	}
+	else if bytes < 1_048_576 {
+		return format!("{:.1}KB", bytes as f64 / 1024.0)
+	}
+	else if bytes < 1_073_741_824 {
+		return format!("{:.1}MB", bytes as f64 / 1_048_576.0)
+	}
+	else if bytes < 1_099_511_627_776 {
+		return format!("{:.1}GB", bytes as f64 / 1_073_741_824.0)
+	}
+	else {
+		return format!("{:.1}TB", bytes as f64 / 1_099_511_627_776.0)
+	}
+}
+
 /// Takes a path and a base path from Windows or Linux, and outputs a path relative to the base path
 /// using "/" as the seperator irrespective of the OS
 pub fn path_to_agnostic_relative(path: &Path, base: &Path) -> String {
@@ -89,6 +107,19 @@ mod tests {
 		let extension = "newext";
 		let expected = PathBuf::from("/home/ray/five/eight/six.newext");
 		let result = add_extension(path, extension);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_format_bytes_kb() {
+		let expected = String::from("976.6KB");
+		let result = format_bytes(1_000_000);
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn test_format_bytes_mb() {
+		let expected = String::from("953.7MB");
+		let result = format_bytes(1_000_000_000);
         assert_eq!(result, expected);
     }
 }
