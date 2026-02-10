86c52208c594ef5f82a0e5d676e416a8779ba4fe
Ray <raymond.gabriel@gmail.com>
2026-02-10 14:00:02 +1300

    add capture groups and borrowed inputs

--- src/regex.rs
+++ src/regex.rs
@@ -1,16 +1,27 @@
 ﻿use regex::Regex;
 
-pub fn match_to_string(string_to_search:&str, re:Regex, capturing_group:Option<usize>) -> Option<String> {
-	//returns the first capturing group to String
+pub fn match_to_string(string_to_search:&str, re:&Regex) -> Option<String> {
+    match re.find(string_to_search) {
+        Some(mat) => Some(mat.as_str().to_string()),
+        None => None
+    }
+}
+
+pub fn match_group_to_string(string_to_search:&str, re:&Regex, capturing_group:Option<usize>) -> Option<String> {
     re.captures(string_to_search)
-        .and_then(|caps| caps.get(capturing_group.unwrap_or(0))) // first capture group
+        .and_then(|caps| caps.get(capturing_group.unwrap_or(0)))
         .map(|m| m.as_str().to_string())
 }
 
-pub fn matches_to_vec(string_to_search:&str, re:Regex) -> Vec<String> {
-    re
-        .find_iter(string_to_search)
+pub fn matches_to_vec(string_to_search:&str, re:&Regex) -> Vec<String> {
+    re.find_iter(string_to_search)
         .map(|m| m.as_str().to_string())
+        .collect()
+}
+
+pub fn matches_group_to_vec(string_to_search:&str, re:&Regex, capturing_group:Option<usize>) -> Vec<String> {
+    re.captures_iter(string_to_search)
+        .filter_map(|caps| Some(caps.get(capturing_group.unwrap_or(0))?.as_str().to_string()))
         .collect()
 }
 
@@ -20,25 +31,40 @@ mod tests {
 
 	#[test]
     fn test_match_to_string() {
-        let re = Regex::new(r"\(([^)]*)").unwrap();
+        let re = Regex::new(r"\(.*\)").unwrap();
         let string_to_search = "![name](image/path/x.png)";
-        let expected = String::from("image/path/x.png");
-        assert_eq!(match_to_string(string_to_search, re, Some(1)), Some(expected));
+        let expected = String::from("(image/path/x.png)");
+        assert_eq!(match_to_string(string_to_search, &re), Some(expected));
     }
 
 	#[test]
     fn test_match_to_string_none() {
-        let re = Regex::new(r"\(([^)]*)").unwrap();
+        let re = Regex::new(r"\(.*\)").unwrap();
         let string_to_search = "![name]image/path/x.png";
-        assert_eq!(match_to_string(string_to_search, re, Some(1)), None);
+        assert_eq!(match_to_string(string_to_search, &re), None);
     }
 
 	#[test]
-    fn test_match_to_string_nocap() {
+    fn test_match_group_to_string() {
+        let re = Regex::new(r"\(([^)]*)").unwrap();
+        let string_to_search = "![name](image/path/x.png)";
+        let expected = String::from("image/path/x.png");
+        assert_eq!(match_group_to_string(string_to_search, &re, Some(1)), Some(expected));
+    }
+
+	#[test]
+    fn test_match_group_to_string_none() {
+        let re = Regex::new(r"\(([^)]*)").unwrap();
+        let string_to_search = "![name]image/path/x.png";
+        assert_eq!(match_group_to_string(string_to_search, &re, Some(1)), None);
+    }
+
+	#[test]
+    fn test_match_group_to_string_nocap() {
         let re = Regex::new(r"\(.*").unwrap();
         let string_to_search = "![name](image/path/x.png)";
         let expected = String::from("(image/path/x.png)");
-        let result = match_to_string(string_to_search, re, None);
+        let result = match_group_to_string(string_to_search, &re, None);
         assert_eq!(result, Some(expected));
     }
 
@@ -47,7 +73,7 @@ mod tests {
         let re = Regex::new(r"!\[.*?\]\(.*?\)").unwrap();
         let string_to_search = "blah ![name](image/path/x.png) blah blah ![name](image/path/y.png) blah";
         let expected = vec![String::from("![name](image/path/x.png)"), String::from("![name](image/path/y.png)")];
-        assert_eq!(matches_to_vec(string_to_search, re), expected);
+        assert_eq!(matches_to_vec(string_to_search, &re), expected);
     }
 
 	#[test]
@@ -55,6 +81,15 @@ mod tests {
         let re = Regex::new(r"!\[.*?\]\(.*?\)").unwrap();
         let string_to_search = "blah [name](image/path/x.png) blah blah ![name]image/path/y.png) blah";
         let expected: Vec<String> = vec![];
-        assert_eq!(matches_to_vec(string_to_search, re), expected);
+        assert_eq!(matches_to_vec(string_to_search, &re), expected);
     }
+
+	#[test]
+    fn test_matches_group_to_vec() {
+        let re = Regex::new(r"\(([^)]*)").unwrap();
+        let string_to_search = "blah ![name](image/path/x.png) blah blah ![name](image/path/y.png) blah";
+        let expected = vec![String::from("image/path/x.png"), String::from("image/path/y.png")];
+        assert_eq!(matches_group_to_vec(string_to_search, &re, Some(1)), expected);
+    }
+
 }
