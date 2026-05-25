e7a1af71f3c291bf1bca21cdb3535d6331cd494b
Ray <raymond.gabriel@gmail.com>
2026-05-25 14:26:17 +1200

    add string_from_utf16_as_vec_u8

--- src/strings.rs
+++ src/strings.rs
@@ -28,6 +28,15 @@ pub fn vec_u8_as_hex(data: &[u8], is_upper: bool, sep: &str) -> String {
     }
 }
 
+pub fn string_from_utf16_as_vec_u8(utf16_data:&[u8]) -> String {
+    String::from_utf16_lossy(
+        &utf16_data
+            .chunks_exact(2)
+            .map(|chunk| u16::from_le_bytes([chunk[0], chunk[1]]))
+            .collect::<Vec<u16>>()
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,5 +63,11 @@ mod tests {
     fn test_vec_u8_as_hex_sep_upper() {
         let input: Vec<u8> = vec![0xde, 0xad, 0xbe, 0xef];
         assert_eq!(vec_u8_as_hex(&input, true, "-"), "DE-AD-BE-EF");
+    }
+
+    #[test]
+    fn test_string_from_utf16_as_vec_u8() {
+        let input: Vec<u8> = vec![0x63, 0x00, 0x6F, 0x00, 0x6E, 0x00, 0x74, 0x00, 0x61, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x65, 0x00, 0x72, 0x00, 0x54, 0x00, 0x65, 0x00, 0x6D, 0x00, 0x70, 0x00, 0x65, 0x00, 0x72, 0x00, 0x61, 0x00, 0x74, 0x00];
+        assert_eq!(string_from_utf16_as_vec_u8(&input), "containerTemperat");
     }
 }
